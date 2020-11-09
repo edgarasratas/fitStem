6306338a6ecc028dcafdd19d6349ce42e7458bb5
Step1st <Step1st@users.noreply.github.com>
2020-11-09 09:51:41 +0200

    Navigation Bar

--- app/src/main/java/com/example/stemfit3/MainScreen.java
+++ app/src/main/java/com/example/stemfit3/MainScreen.java
@@ -2,11 +2,17 @@ package com.example.stemfit3;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainScreen extends AppCompatActivity {
     @Override
@@ -14,7 +20,11 @@ public class MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
-
 }
+
+
 
