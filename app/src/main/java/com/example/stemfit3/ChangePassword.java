11ebc2823ca55e1dffe2e283641102b2e2747718
edgarasratas <73912309+edgarasratas@users.noreply.github.com>
2020-12-24 05:35:13 +0200

    You can change passwords now!

--- app/src/main/java/com/example/stemfit3/ChangePassword.java
+++ app/src/main/java/com/example/stemfit3/ChangePassword.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.accessibilityservice.AccessibilityGestureEvent;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -34,6 +35,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -51,13 +53,78 @@ import java.sql.Array;
 
 public class ChangePassword extends AppCompatActivity {
 
+    private Button mSave;
+    private EditText currentPassword;
+    private EditText newPassword;
+    private EditText confirmPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+
+        mSave = (Button)findViewById(R.id.Save);
+        currentPassword = (EditText) findViewById(R.id.CurrentPassword);
+        newPassword = (EditText)findViewById(R.id.NewPassword);
+        confirmPassword = (EditText)findViewById(R.id.ConfirmPassword);
+
+        mSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String oldPass = currentPassword.getText().toString().trim();
+                if(oldPass.isEmpty()) {
+                    currentPassword.setError("This field is required");
+                    currentPassword.requestFocus();
+                    return;
+                }
+                String newPass = newPassword.getText().toString().trim();
+                if(newPass.isEmpty()) {
+                    newPassword.setError("This field is required");
+                    newPassword.requestFocus();
+                    return;
+                }
+                String confirmPass = confirmPassword.getText().toString().trim();
+                if(confirmPass.isEmpty()) {
+                    confirmPassword.setError("This field is required");
+                    confirmPassword.requestFocus();
+                    return;
+                }
+                if(newPass.length() < 8) {
+                    newPassword.setError("Password length must be at least 8 characters...");
+                    newPassword.requestFocus();
+                }
+                if(!newPass.equals(confirmPass)) {
+                    confirmPassword.setError("Passwords do not match...");
+                    confirmPassword.requestFocus();
+                }
+
+                confirmPassword(oldPass, newPass, confirmPass);
+            }
+
+            });
     }
 
-    public void exitChangePassword(View v){
+    private void confirmPassword(String oldPass, String newPass, String confirmPass) {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        AuthCredential authCredential = EmailAuthProvider.getCredential(user.getEmail(), oldPass);
+        user.reauthenticate(authCredential)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if((task.isSuccessful()) && (newPass.equals(confirmPass) && (newPass.length() >= 8))) {
+                            user.updatePassword(newPass);
+                            finish();
+                            Toast.makeText(getApplicationContext(), "Password Updated", Toast.LENGTH_SHORT).show();
+                        }
+                        else if(!task.isSuccessful()) {
+                            currentPassword.setError("Current password is incorrect...");
+                            currentPassword.requestFocus();
+                        }
+                    }
+                });
+    }
+    public void exitChangePassword (View v) {
         finish();
     }
 }
